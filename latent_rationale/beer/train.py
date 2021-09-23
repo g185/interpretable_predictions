acd77e3025db5c8ccbfab211ed251d41efde9270
Giuliano Martinelli <51946934+g185@users.noreply.github.com>
2021-09-23 15:25:48 +0200

    c

--- latent_rationale/beer/train.py
+++ latent_rationale/beer/train.py
@@ -22,7 +22,7 @@ from latent_rationale.beer.evaluate import \
     evaluate_rationale, get_examples, evaluate_loss
 from latent_rationale.common.util import make_kv_string
 wandb.login()
-wandb.init(name="bastings_0")
+wandb.init(project="my-test-project", name="bastings_0")
 
 def train():
     """
